79f5203a195d85fb67055e90721f12cd06d44d2d
Henri da Silva <hpsilva@pm.me>
2025-06-26 05:56:52 -0300

    dasd

---
+++ Chapter 3/SimpleCSharpApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// Display a message and wait for Enter key to be pressed.
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine(args.Length);
+
+// Process any incoming args.
+for (int i = 0; i < args.Length; i++)
+{
+    Console.WriteLine("Arg: {0}", args[i]);
+}
+Console.ReadLine();
+
+// Return an arbitrary error code.
+return 0;
