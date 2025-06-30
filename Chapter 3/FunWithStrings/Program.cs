4baeb483f31c617bce01ba1f3fdd9a4d40223e8d
Henri da Silva <hpsilva@pm.me>
2025-06-30 13:08:09 -0300

    Fix string containment check to use character literal in BasicStringFunctionality

--- Chapter 3/FunWithStrings/Program.cs
+++ Chapter 3/FunWithStrings/Program.cs
@@ -16,7 +16,7 @@ static void BasicStringFunctionality()
     Console.WriteLine("firstName has {0} characters.", firstName.Length);
     Console.WriteLine("firstName in uppercase: {0}", firstName.ToUpper());
     Console.WriteLine("firstName in lowercase: {0}", firstName.ToLower());
-    Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains("y"));
+    Console.WriteLine("firstName contains the letter y?: {0}", firstName.Contains('y'));
     Console.WriteLine("New first name: {0}", firstName.Replace("dy", ""));
     Console.WriteLine();
 }
